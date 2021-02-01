f9afd8db7819dba9f8faedbb132b2a1e24aed1bc
richen604 <56615615+richen604@users.noreply.github.com>
2021-01-31 16:37:29 -0800

    Exercise 8.7 /apollo-library

--- apollo-library/index.js
+++ apollo-library/index.js
@@ -107,6 +107,10 @@ const typeDefs = gql`
       id: Int
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int
+    ): Author
   }
 `;
 
@@ -147,6 +151,14 @@ const resolvers = {
       const book = {...args, id: uuidv4()}
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+        const author = authors.find(author => author.name === args.name)
+        if(!author) return null
+
+        const updatedAuthor = {...author, born: args.setBornTo }
+        authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+        return updatedAuthor
     }
   }
 };
